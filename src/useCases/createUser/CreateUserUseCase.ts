dca2e0bd3b7631bbe40ab316a4fe4b759ca19946
wilsinneto <wngaspar10@gmail.com>
2020-11-13 12:54:31 -0300

    Implementing CreateUserUseCase and applicatying refactory

--- src/useCases/createUser/CreateUserUseCase.ts
+++ src/useCases/createUser/CreateUserUseCase.ts
@@ -1,13 +1,17 @@
+import { User } from "../../entities/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
 
-export class CreateUserUseCase {
-  private usersRepository: IUsersRepository;
-  
-  constructor(usersRepository: IUsersRepository) {
-    this.usersRepository = usersRepository
-  }
+export class CreateUserUseCase {  
+  constructor(private usersRepository: IUsersRepository) {}
 
-  async execute() {
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(data.email)
     
+    if (userAlreadyExists) throw new Error("User already exists.")
+
+    const user = new User(data)
+
+    await this.usersRepository.save(user)
   }
 }
